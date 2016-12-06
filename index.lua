2eb825940310af03074a77861a11fcc158c7d1ee
gnmmarechal <gnmpolicemata91@gmail.com>
2016-12-06 01:16:32 +0000

    Pushing stuff before release

--- index.lua
+++ index.lua
@@ -14,15 +14,41 @@
 --
 --
 
+debugMode = false
+debugAllowed = false
+verString = "Starfield Vita v0.1 - by gnmmarechal"
+
 -- Set CPU clock to 444MHz
 System.setCpuSpeed(444)
-FPS_TARGET = 60
+local FPS_TARGET = 60
+local pad = Controls.read()
+local oldpad = pad
+System.createDirectory("ux0:/data")
+System.createDirectory("ux0:/data/Starfield-Vita")
 
 -- Init sound
 Sound.init()
-themeA = Sound.openMp3("app0:/sound/bensound-scifi.mp3")
+themeA = Sound.openMp3("app0:/res/sound/bensound-scifi.mp3")
 
 running = true
+
+-- GekiHEN Splash Screen
+splashScreen = Graphics.loadImage("app0:/res/img/splash.png")
+logoImage = Graphics.loadImage("app0:/res/img/logo.png")
+
+splashTimer = Timer.new()
+
+while(Timer.getTime(splashTimer) <= 3000) do
+	Graphics.initBlend()
+	Screen.clear()
+	--
+	Graphics.drawImage(0, 0, splashScreen)
+	--
+	Graphics.termBlend()
+	Screen.flip()
+end
+Timer.destroy(splashTimer)
+
 -- Set PS Vita Resolution
 local width = 960
 local height = 544
@@ -38,10 +64,19 @@ local color = {
 
 -- Important Variables
 local score = 0
+local recordScore = 0
+local scoreFilePath = "ux0:/data/Starfield-Vita/record.dat"
 local lives = 3
 
-local gameLoopCounter = 0;
+local gameLoopCounter = 0
+local lifeTakenTimer = Timer.new()
+local lifeTaken = false
+local curScene = 0
 
+local shipCoords = {
+	x = 40,
+	y = math.floor(height/2) 
+}
 
 
 -- Stars
@@ -58,6 +93,22 @@ end
 
 
 -- Important functions
+
+function readScore(scorePath)
+	if System.doesFileExist(scorePath) then
+		fileStream = io.open(scorePath ,FREAD)
+		recordScore = tonumber(io.read(fileStream, io.size(fileStream)))
+		io.close(fileStream)
+	end	
+end
+function saveScore(scoreVar, scorePath)
+	if System.doesFileExist(scorePath) then
+		System.deleteFile(scorePath)
+	end	
+	fileStream = io.open(scorePath ,FCREATE)
+	io.write(fileStream, scoreVar, string.len(tostring(scoreVar)))
+	io.close(fileStream)	
+end
 local fps_counter = 0
 local fps_timer = Timer.new()
 local function reachedFPScap(x)
@@ -70,58 +121,210 @@ local function reachedFPScap(x)
 	return (fps_counter >= x)
 end
 
+function drawPlayer(coordX, coordY)
+	Graphics.fillRect(coordX - 9, coordX + 9, coordY - 4, coordY + 4, color.red) 
+end
+
+function getDist(x1, y1, x2, y2)
+	return (math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))))
+end
+
 -- Game functions
+function gameOver()
+	if score > recordScore then
+		recordScore = score
+		saveScore(recordScore, scoreFilePath)
+	end
+	score = 0
+	lives = 3
+	
+	gameLoopCounter = 0
+	lifeTakenTimer = Timer.new()
+	lifeTaken = false
+	curScene = 0
+	for i = 1, STARS do
+		newStars[i] = {width, math.random(height), math.sqrt(math.random(4))}
+	end	
+end
+function gameMenu()
+	Graphics.drawImage(210, 50, logoImage)
+	Graphics.debugPrint(0, 0, verString, color.royalBlue)
+	Graphics.debugPrint(0, 20, "Record: "..recordScore, color.royalBlue)
+	Graphics.debugPrint(0, 40, "Press X to start", color.royalBlue)
+	if Controls.check(pad, SCE_CTRL_CROSS) and (not Controls.check(oldpad, SCE_CTRL_CROSS)) then
+		curScene = 1
+		System.wait(100)
+	end
+end
 function starfield()
 	gameLoopCounter = gameLoopCounter + 1
-	
+	Graphics.debugPrint(0, 0, "Points: "..score, color.royalBlue)
+	Graphics.debugPrint(0, 15, "Lives: "..lives, color.royalBlue)	
 	if gameLoopCounter == 1 then
 		gameTimer = Timer.new()
 	end
-	if gameTimer >= 5000 then
+	-- Draw Ship
+	drawPlayer(shipCoords.x, shipCoords.y)
+	
+	-- Control ship (Analogs)
+	analogX, analogY = Controls.readLeftAnalog()
+	if analogY <= 120 then
+		--shipCoords.y = shipCoords.y - (math.floor(analogY/20))
+		if analogY <= 60 then
+			valToChange = -2
+		else
+			valToChange = -1
+		end
+		shipCoords.y = shipCoords.y + valToChange
+	end
+	if analogY >= 135 then
+		--shipCoords.y = shipCoords.y + (math.floor(analogY/20))
+		if analogY >= 195 then
+			valToChange = 2
+		else
+			valToChange = 1
+		end
+		shipCoords.y = shipCoords.y + valToChange		
+	end
+	if analogX <= 115 then
+		--shipCoords.y = shipCoords.y - (math.floor(analogY/20))
+		if analogX <= 120 then
+			valToChange = -2
+		else
+			valToChange = -1
+		end
+		shipCoords.x = shipCoords.x + valToChange			
+	end
+	if analogX >= 140 then
+		--shipCoords.y = shipCoords.y + (math.floor(analogY/20))
+		if analogX >= 195 then
+			valToChange = 2
+		else
+			valToChange = 1
+		end
+		shipCoords.x = shipCoords.x + valToChange	
+	end	
+	
+	-- Control Ship (DPAD)
+	if Controls.check(pad, SCE_CTRL_UP) then
+		shipCoords.y = shipCoords.y - 1
+	end
+	if Controls.check(pad, SCE_CTRL_DOWN) then
+		shipCoords.y = shipCoords.y + 1
+	end
+	if Controls.check(pad, SCE_CTRL_LEFT) then
+		shipCoords.x = shipCoords.x - 1
+	end
+	if Controls.check(pad, SCE_CTRL_RIGHT) then
+		shipCoords.x = shipCoords.x + 1
+	end	
+	if shipCoords.x <= 0 then
+		shipCoords.x = 0
+	end
+	if shipCoords.x >= width then
+		shipCoords.x = width
+	end
+	if shipCoords.y >= height then
+		shipCoords.y = height
+	end
+	if shipCoords.y <= 0 then
+		shipCoords.y = 0
+	end
+	if debugMode then
+		Graphics.debugPrint(0, 30, "shipX: "..shipCoords.x.." shipY: "..shipCoords.y.." Analog X: "..analogX.." Analog Y: "..analogY, color.royalBlue)
+	end
+	-- Don't spawn stars for the first 5 seconds.
+	if Timer.getTime(gameTimer) >= 5000 then
 		for i = 1, STARS do
 			Graphics.fillCircle(newStars[i][1], newStars[i][2], 4, color.white)
 			
 			newStars[i][1] = newStars[i][1] - newStars[i][3];
 			if newStars[i][1] < 0 then -- Spawn a new star for each star that goes out of the screen
-				newStars[i] = {width, math.random(height), math.sqrt(math.random(10))}
+				newStars[i] = {width, math.random(height), math.sqrt(math.random(4))}
 			end
+			
+			-- Check collision
+			if (Timer.getTime(lifeTakenTimer) >= 300) then
+				Timer.pause(lifeTakenTimer)
+				Timer.reset(lifeTakenTimer)
+				lifeTaken = false
+			end
+			if (getDist(newStars[i][1], newStars[i][2], shipCoords.x, shipCoords.y) <= 4) then
+			
+				if not lifeTaken then
+					lives = lives - 1			
+					if lives <= 0 then
+						curScene = 2
+					end	
+					Timer.resume(lifeTakenTimer)
+					lifeTaken = true
+				end
+			end
+			
+
+			
 		end
+		score = score + math.floor(Timer.getTime(gameTimer)/1000 * FPS_TARGET/60)
+	else
+		Timer.pause(lifeTakenTimer)
+		Timer.reset(lifeTakenTimer)
 	end
 	
 	
 end
-
+function game(scene)
+	if scene == 0 then
+		gameMenu()
+	elseif scene == 1 then
+		starfield()
+	elseif scene == 2 then
+		gameOver()
+	end	
+end
 
 for i = 1, STARS do
-	newStars[i] = {width, height, math.random(10)}
+	newStars[i] = {width, math.random(height), math.sqrt(math.random(4))}
 end
 Sound.play(themeA, LOOP)
 -- Starfield
 
 
+readScore(scoreFilePath)
 
 while running do
 
 	while not reachedFPScap(FPS_TARGET) do
+		pad = Controls.read()
 		Graphics.initBlend()
 		Screen.clear()
 		--
-		starfield()
-		Graphics.debugPrint(0,0, "FPS:"..FPS_TARGET, color.royalBlue)
-		
+		game(curScene)
 		--
 		Graphics.termBlend()
 		Screen.flip()
-		if Controls.check(Controls.read(), SCE_CTRL_DOWN) then
-			FPS_TARGET = FPS_TARGET - 1
-			if FPS_TARGET <= 0 then
-				FPS_TARGET = 1
+		
+		
+		if debugMode then
+			if Controls.check(pad, SCE_CTRL_LTRIGGER) then
+				FPS_TARGET = FPS_TARGET - 1
+				if FPS_TARGET <= 0 then
+					FPS_TARGET = 1
+				end
+			end
+			if Controls.check(pad, SCE_CTRL_RTRIGGER) then
+				FPS_TARGET = FPS_TARGET + 1
 			end
 		end
-		if Controls.check(Controls.read(), SCE_CTRL_UP) then
-			FPS_TARGET = FPS_TARGET + 1
-		end
+		
+		if Controls.check(pad, SCE_CTRL_SELECT) and debugAllowed then
+			debugMode = not debugMode
+		end	
 	end
+	
+	if Controls.check(pad, SCE_CTRL_START) and (not Controls.check(oldpad, SCE_CTRL_START)) then
+		System.takeScreenshot("ux0:/data/file.jpg", true, 0)
+	end
+	oldpad = pad
 end
 
 Sound.term()
